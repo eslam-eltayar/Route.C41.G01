ce325791996eae448b8a62d847a381ff2157ab9e
Eslam Eltayar <tayaruwk@gmail.com>
2024-03-24 16:55:59 +0200

    Part 09 Employee Controller Views

--- Route.C41.G01.PL/Startup.cs
+++ Route.C41.G01.PL/Startup.cs
@@ -40,7 +40,7 @@ namespace Route.C41.G01.PL
             });
 
             services.AddScoped<IDepartmentRepository, DepartmentRepository>();
-
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
