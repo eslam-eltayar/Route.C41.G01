55fa23b3393a93ee3f95c70124aea1c254fea9ef
Eslam Eltayar <tayaruwk@gmail.com>
2024-04-26 21:47:43 +0300

    Account Controller - Add SignOut

--- Route.C41.G01.PL/Controllers/AccountController.cs
+++ Route.C41.G01.PL/Controllers/AccountController.cs
@@ -8,112 +8,124 @@ using System.Threading.Tasks;
 
 namespace Route.C41.G01.PL.Controllers
 {
-	public class AccountController : Controller
-	{
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly SignInManager<ApplicationUser> _signInManager;
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-		public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
-		{
-			_userManager = userManager;
-			_signInManager = signInManager;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
 
-		}
+        }
 
-		#region Sign Up (Register)
+        #region Sign Up (Register)
 
-		[HttpGet]
-		public IActionResult SignUp()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> SignUp(SignUpViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var user = await _userManager.FindByNameAsync(model.UserName);
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
 
-				if (user is null)
-				{
-					user = new ApplicationUser()
-					{
-						FName = model.FirstName,
-						LName = model.LastName,
-						UserName = model.UserName,
-						Email = model.Email,
-						IsAgree = model.IsAgree
-					};
+                if (user is null)
+                {
+                    user = new ApplicationUser()
+                    {
+                        
+                        FName = model.FirstName,
+                        LName = model.LastName,
+                        UserName = model.UserName,
+                        Email = model.Email,
+                        IsAgree = model.IsAgree
+                    };
 
-					var Result = await _userManager.CreateAsync(user, model.Password);
+                    var Result = await _userManager.CreateAsync(user, model.Password);
 
-					if (Result.Succeeded)
-					{
-						return RedirectToAction(nameof(SignIn));
-					}
+                    if (Result.Succeeded)
+                    {
+                        return RedirectToAction(nameof(SignIn));
+                    }
 
-					foreach (var error in Result.Errors)
-					{
-						ModelState.AddModelError(string.Empty, error.Description);
+                    foreach (var error in Result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
 
-					}
+                    }
 
-				}
-				ModelState.AddModelError(string.Empty, "This userName is Already in use for another Account");
+                }
+                ModelState.AddModelError(string.Empty, "This userName is Already in use for another Account");
 
-			}
-			return View(model);
-		}
+            }
+            return View(model);
+        }
 
-		#endregion
+        #endregion
 
-		#region Sign In
+        #region Sign In
 
-		[HttpGet]
-		public IActionResult SignIn()
-		{
-			return View();
-		}
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> SignIn(SignInViewModel model)
-		{
-			if (ModelState.IsValid)
-			{
-				var user = await _userManager.FindByEmailAsync(model.Email);
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
 
-				if (user is not null)
-				{
-					var flage = await _userManager.CheckPasswordAsync(user, model.Password);
+                if (user is not null)
+                {
+                    var flage = await _userManager.CheckPasswordAsync(user, model.Password);
 
-					if (flage)
-					{
-						var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
-
-
-						if (result.IsLockedOut)
-							ModelState.AddModelError(string.Empty, "Your Account is Locked !");
+                    if (flage)
+                    {
+                        var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
 
 
-						if (result.Succeeded)
-							return RedirectToAction(nameof(HomeController.Index), "Home");
-
-						if (result.IsNotAllowed)
-							ModelState.AddModelError(string.Empty, "Your Account is not Confirmed yet!!");
+                        if (result.IsLockedOut)
+                            ModelState.AddModelError(string.Empty, "Your Account is Locked !");
 
 
-					}
+                        if (result.Succeeded)
+                            return RedirectToAction(nameof(HomeController.Index), "Home");
 
-				}
+                        if (result.IsNotAllowed)
+                            ModelState.AddModelError(string.Empty, "Your Account is not Confirmed yet!!");
 
-				ModelState.AddModelError(string.Empty, "Invalid Login");
 
-			}
-			return View(model);
-		}
+                    }
 
-		#endregion
-	}
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login");
+
+            }
+            return View(model);
+        }
+
+        #endregion
+
+        #region Sign Out
+
+        public new async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(SignIn));
+        }
+
+        #endregion
+    }
 
 }
